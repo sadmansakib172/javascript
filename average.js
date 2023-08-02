a150c3931373608401656299ca8282527f527144
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-08-02 17:12:02 +0600

    average problem done

---
+++ average.js
@@ -0,0 +1,9 @@
+function make_avg(num1, num2, num3){
+ const avg = (num1+ num2 + num3)/3;
+ return avg;
+}
+const number1 = 100;
+const number2 = 56;
+const number3 = 54;
+ const result = make_avg(number1, number2, number3);
+ console.log(result);
