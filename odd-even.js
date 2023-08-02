20be16e00b62d42c558a032ef28e70ca6107b181
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-08-02 18:02:53 +0600

    has return has parameter done

--- odd-even.js
+++ odd-even.js
@@ -1,10 +1,14 @@
 function odd_even(number){
+    if(typeof number !== "number" || !Number.isInteger(number)){
+        throw new Error("Input must be an integer number: ");
+    }
  if(number % 2 == 0){
-    console.log('number is even'); 
+    return "even";
  }
  else{
-    console.log('number is odd');
+    return "odd";
  }
 }
 
-odd_even(35);
+let result = odd_even(36);
+console.log(result);
