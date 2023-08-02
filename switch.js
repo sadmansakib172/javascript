6ff734239edbc46213c46990496591729fcf1f02
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-08-02 18:08:21 +0600

    switch problem done

---
+++ switch.js
@@ -0,0 +1,12 @@
+const signal = "green";
+switch (signal) {
+    case 'green':
+        console.log('I will cross the road');
+        break;
+    case "red":
+        console.log("I will stop at the signal");
+        break;
+    case "yellow":
+        console.log("I will ready for cross the road");
+        break;
+}
