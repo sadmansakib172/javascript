2f68896a2fe37ad187603a51687a3b22b3ac5c74
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-08-03 10:56:02 +0600

    leap year done

---
+++ leap-year-find.js
@@ -0,0 +1,14 @@
+function leapYearFind(array){
+    let newArray = [];
+   for(let i = 0; i< array.length; i++){
+    let singleYear = array[i];
+    if(singleYear % 4 == 0 && singleYear % 100 == 0 && singleYear % 400 == 0){
+      newArray.push(singleYear);
+    }
+   }
+   return newArray;
+}
+
+let array = [2023, 2024, 2025, 2028, 2030,2000];
+let result = leapYearFind(array);
+console.log(result);
