309844f085c4db3c5eccbcf2c192a230da3be47f
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-08-03 17:05:59 +0600

    leap year correction added

--- leap-year-find.js
+++ leap-year-find.js
@@ -2,7 +2,7 @@ function leapYearFind(array){
     let newArray = [];
    for(let i = 0; i< array.length; i++){
     let singleYear = array[i];
-    if(singleYear % 4 == 0 && singleYear % 100 == 0 && singleYear % 400 == 0){
+    if(singleYear % 4 == 0 || singleYear % 100 == 0 || singleYear % 400 == 0){
       newArray.push(singleYear);
     }
    }
